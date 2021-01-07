f5ae24ae6f3c98cdcff332e3a67b84b1cc7a617d
Alcala, Jose Luis <joseluis.alcala@adidas.com>
2021-01-07 13:53:28 +0100

    FIRST draft

--- src/efergyAccesory.ts
+++ src/efergyAccesory.ts
@@ -1,31 +1,32 @@
 import {
-  AccessoryConfig,
-  AccessoryPlugin,
-  API,
-  CharacteristicEventTypes,
-  CharacteristicGetCallback,
-  CharacteristicSetCallback,
-  CharacteristicValue,
-  HAP,
-  Logging,
-  Service
+	AccessoryConfig,
+	AccessoryPlugin,
+	API,
+	CharacteristicEventTypes,
+	CharacteristicGetCallback,
+	CharacteristicSetCallback,
+	CharacteristicValue,
+	Formats,
+	HAP,
+	Perms,
+	Logging,
+	Service
 } from "homebridge";
 
 let hap: HAP;
-
 /*
  * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  hap = api.hap;
-  api.registerAccessory("EfergyAccessory", EfergyAccessory);
+	hap = api.hap;
+	api.registerAccessory("EfergyAccessory", EfergyAccessory);
 };
 
 class EfergyAccessory implements AccessoryPlugin {
-  private readonly log: Logging;
-  private readonly name: string;
+	private readonly log: Logging;
+	private readonly name: string;
 
-  private readonly powerConsumptionService: Service;
+	private readonly powerConsumptionService: Service;
 	private readonly informationService: Service;
 
 
@@ -36,14 +37,22 @@ class EfergyAccessory implements AccessoryPlugin {
 
 		this.powerConsumptionService = new hap.Service.PowerManagement(this.name);
 
-		this.powerConsumptionService.getCharacteristic(hap.Characteristic.ServiceLabelIndex).on(CharacteristicEventTypes.GET, (callback:CharacteristicGetCallback) => {
-			log.info(' Get Called')
-			callback(undefined,'800kWh');
+		this.powerConsumptionService.getCharacteristic(hap.Characteristic.WakeConfiguration)
+			.on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+				log.info(' Get Called')
+				callback(undefined);
+			})
+		const s = new hap.Characteristic('s','s', { format: Formats.STRING, perms: [Perms.READ, Perms.NOTIFY]})
+		this.powerConsumptionService.addCharacteristic(s);
+
+		s.on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+			log.info(' Get Called 2')
+			callback(undefined, "800kWh");
 		})
 
-    this.informationService = new hap.Service.AccessoryRuntimeInformation()
-     .setCharacteristic(hap.Characteristic.Manufacturer, 'Efergy')
-     .setCharacteristic(hap.Characteristic.Model, 'Elite Classic');
+		this.informationService = new hap.Service.AccessoryRuntimeInformation()
+			.setCharacteristic(hap.Characteristic.Manufacturer, 'Efergy')
+			.setCharacteristic(hap.Characteristic.Model, 'Elite Classic');
 
 		log.info('Finished initializaion of efergy')
 	}
@@ -53,6 +62,7 @@ class EfergyAccessory implements AccessoryPlugin {
 	}
 
 	getServices(): Service[] {
-		return [this.informationService,  this.powerConsumptionService]
+		return [this.informationService, this.powerConsumptionService]
 	}
 }
+
